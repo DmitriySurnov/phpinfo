899640e65678a42f6dfe4b2187fd6d78b6b57a49
DmitriySurnov <79646813832@yandex.ru>
2023-11-11 12:26:13 +0300

    Задание выполнено

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>phpinfo</title>
+    <link rel="stylesheet" href="Styles/footer.css">
+</head>
+<body>
+<? phpinfo(); ?>
+</body>
+<footer>
+    <div>
+        <span> Crated by Surnov Dmitriy (2023) </span>
+    </div>
+</footer>
+</html>
+
